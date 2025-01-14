bc0ae214c55c1c715bd1337c8a8aa39ecea0b7b0
yeuidowanter <ggp06271@naver.com>
2025-01-14 16:50:01 +0900

    legend

---
+++ src/main/java/com/example/chat/HelloMessage.java
@@ -0,0 +1,17 @@
+package com.example.chat;
+
+public class HelloMessage {
+    private String name;
+
+    public HelloMessage(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
